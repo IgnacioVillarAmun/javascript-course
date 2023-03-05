0d8c345aa40c95290ec54efc9c4670a4c2b5ec75
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2023-03-04 19:47:02 -0700

    progress

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -487,20 +487,51 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(overallBalance);
 
 // String sort
-const owner = ['Jonas', 'Zach', 'Adam', 'Martha'];
-console.log(owner.sort());
-console.log(owner);
+// const owner = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// console.log(owner.sort());
+// console.log(owner);
 
-// Number sort
-console.log(movements);
+// // Number sort
+// console.log(movements);
 
-// return < 0, A, B (Keep order)
-// return > 0, B, A (switch order)
+// // return < 0, A, B (Keep order)
+// // return > 0, B, A (switch order)
 
-// Ascending
-movements.sort((a, b) => a - b);
-console.log(movements);
+// // Ascending
+// movements.sort((a, b) => a - b);
+// console.log(movements);
 
-// Descending
-movements.sort((a, b) => b - a);
-console.log(movements);
+// // Descending
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+// // Empty arrays + fill method
+// const x = new Array(7);
+// console.log(x);
+// // console.log(x.map(() => 5));
+// x.fill(1, 3, 5);
+// console.log(x);
+
+// arr.fill(23, 2, 6);
+// console.log(arr);
+
+// // Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y);
+
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z);
+
+// // const z100 = Array.from({ length: 100 }, (_, i) => i + 1);
+// // console.log(z100);
+
+// labelBalance.addEventListener('click', function () {
+//   const movementsUI = Array.from(
+//     document.querySelectorAll('.movements__value'),
+//     el => el.textContent.replace('€', '')
+//   );
+//   console.log(movementsUI);
+// });
